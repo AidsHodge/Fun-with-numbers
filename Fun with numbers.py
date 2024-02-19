1f8aca34f3690793f9e34c3534ede9d46a53fa6b
AidsHodge <159495020+AidsHodge@users.noreply.github.com>
2024-02-19 14:44:50 +1100

    Update Fun with numbers.py

--- Fun with numbers.py
+++ Fun with numbers.py
@@ -5,3 +5,26 @@
       print(" (B) Plot numbers")
       print(" (C) Check overall stats")
       print("\n (X) Save and exit")
+      choice = input("/nChoice:").upper()
+
+      if choice == "A":
+          number feature ()
+        elif choice == "B":
+              plotter()
+        elif choice == "C":
+              stats()
+        elif choice== "X":
+            exit_flag = True
+def number_features():
+  """Displayes features of the chosen number."""
+  print("This is the number features sub-routine")
+
+def plotter():
+  """Plotts numbers on a graph."""
+  print("This is the plotter sub-routine")
+
+def stats():
+  """Displays statistics of numbers used in the program."""
+  print("This is the stats sub-routine")
+
+main()
